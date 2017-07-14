bde06e20fa3817941bcd9d0f400d753ece19152b
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-14 17:44:38 -0400

    Fix typoss

--- selectio/models.py
+++ selectio/models.py
@@ -7,11 +7,11 @@ class Person(Base):
     first_name = Column(String(50))         # e.g. Frank, Nick
     last_name = Column(String(50))          # e.g. Matranga
     nickname = Column(String(30))           # e.g. Nick
-    relation = Column(String(50))           # Relation to me e.g. self, brother, friend, classmate
-    gender = Column(String(10))             # Male, Female
-    description = Column(String(250))                   # e.g. 17, 54, 79
+    relation = Column(String(50))           # e.g. self, brother, friend, classmate
+    gender = Column(String(10))             # e.g. Male, Female
+    description = Column(String(250))       # e.g. Coolest guy ever...
 
-    def __init__(self, first_name=None, last_name=None, nickname=None, title=None, relation=None, gender=None, description=None):
+    def __init__(self, first_name=None, last_name=None, nickname=None, relation=None, gender=None, description=None):
         self.first_name = first_name
         self.last_name = last_name
         self.nickname = nickname
