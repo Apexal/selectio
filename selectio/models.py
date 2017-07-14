e9f6ab8ff102dd0090c63ece5a65e0a24d215854
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-14 17:39:43 -0400

    Nicknames

--- selectio/models.py
+++ selectio/models.py
@@ -6,14 +6,16 @@ class Person(Base):
     id = Column(Integer, primary_key=True)  # e.g. 1, 2, 3, 4
     first_name = Column(String(50))         # e.g. Frank, Nick
     last_name = Column(String(50))          # e.g. Matranga
+    nickname = Column(String(30))           # e.g. Nick
     title = Column(String(30))              # e.g. Mr., Mrs., Fr., Dr.
     relation = Column(String(50))           # Relation to me e.g. self, brother, friend, classmate
     gender = Column(String(10))             # Male, Female
-    age = Column(Integer)                   # e.g. 17, 54, 79
+    description = Column(String(250))                   # e.g. 17, 54, 79
 
-    def __init__(self, first_name=None, last_name=None, title=None, relation=None, gender=None, description=None):
+    def __init__(self, first_name=None, last_name=None, nickname=None, title=None, relation=None, gender=None, description=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.nickname = nickname
         self.title = title
         self.relation = relation
         self.gender = gender
--- selectio/views.py
+++ selectio/views.py
@@ -1,6 +1,6 @@
 from selectio import app, db_session
 from selectio.models import *
-from flask import render_template, request, flash
+from flask import render_template, request, redirect, url_for, flash
 
 @app.route('/')
 def index():
@@ -28,13 +28,18 @@ def add_person():
         # Get all values from form
         first_name = request.form['firstName'].strip()
         last_name = request.form['lastName'].strip()
+        nickname = first_name
+
+        if 'nickname' in request.form:
+            nickname = request.form['nickname'].strip()
+
         title = request.form['title'].strip()
         relation = request.form['relation'].strip().lower()
         gender = request.form['gender'].strip().lower()
         description = request.form['description'].strip()
 
         # Create new person
-        new_person = Person(first_name, last_name, title, relation, gender, description)
+        new_person = Person(first_name, last_name, nickname, title, relation, gender, description)
         db_session.add(new_person)
         db_session.commit()
 
